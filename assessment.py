894e4c3a0a32b7e6e124d6d3a36cd0418a62fd47
Carrie Li <carrie.t.li@gmail.com>
2017-04-17 01:37:37 +0000

    Added repr methods and administer method

--- assessment.py
+++ assessment.py
@@ -67,31 +67,76 @@ Part 1: Discussion
 # Create your classes and class methods
 
 class Student(object):
-    """Stores student first and last names, and addresses"""
+    """ Stores student first and last names, and addresses."""
 
     def __init__(self, first_name, last_name, address):
-        """Initializes the class variables to store data"""
+        """ Initializes the class variables to store data"""
 
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
 
     def __repr__(self):
+        """ Returns student name and address stored in dictionary."""
 
+        return "first name: {}, \nlast name: {}, \naddress: {}".format(
+            self.first_name, self.last_name, self.address)
 
 class Question(object):
-    """Stores a question and a correct answer"""
+    """ Stores a question and a correct answer."""
 
     def __init__(self, question, correct_answer):
-        """Initializes the class variables to store data"""
+        """ Initializes the class variables to store data."""
 
         self.question = question
         self.correct_answer = correct_answer
 
+    def __repr__(self):
+        """ Returns dictionary of (question: correct_answer)."""
+
+        return "question: {}, \ncorrect_answer: {}".format(self.question,
+            self.correct_answer)
+
     def ask_and_evaluate(self):
-        """Prints question, prompts user for answer, then evaluates answer."""
+        """ Prints question, prompts user for answer, then evaluates answer."""
 
+        print question
+        answer = raw_input("> ")
+        if answer == correct_answer:
+            return True
+        else:
+            return False
 
+class Exam(object):
+    """Stores data that is a dictionary of (question: correct answers)."""
 
+    def __init__(self, name):
+        """ Initializes the class variables and attribute."""
 
+        self.name = name
+        self.questions_list = [] # list of Question objects
 
+    def __repr__(self):
+        """ Returns a dictionary containting values point to lists of questions
+            and answers."""
+
+            return "name: {}, list of questions: [{}]".format(self.name, 
+                self.questions_list)
+
+    def add_question(self, question, correct_answer):
+        """Adds question and correct_answer to questions list"""
+
+        question_instance = Question(question, correct_answer) # create instance
+        self.questions_list.append(question_instance)
+
+    def administer(self):
+        """ Administers all questions and returns user score. """
+
+        score = 0.00
+
+        num_corr_response = self.ask_and_evaluate()
+        for question in questions_list:
+            if num_corr_response == True,
+                score += 1
+
+        return self.score / len(self.questions_list)
